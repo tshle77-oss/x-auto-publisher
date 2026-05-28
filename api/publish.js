2fa90c34d73d0332a1b160b0f75e006198e4510b
tshle77-oss <tshle77@gmail.com>
2026-05-28 20:25:55 +0300

    Update publish.js

--- api/publish.js
+++ api/publish.js
@@ -9,7 +9,13 @@ return encodeURIComponent(str)
 .replace(/\)/g, "%29");
 }
 
-function generateOAuthHeader(method, url, params, consumerSecret, tokenSecret){
+function generateOAuthHeader(
+method,
+url,
+params,
+consumerSecret,
+tokenSecret
+){
 
 const sortedKeys =
 Object.keys(params).sort();
@@ -37,7 +43,7 @@ crypto
 
 params.oauth_signature = signature;
 
-const authHeader =
+return (
 "OAuth " +
 Object.keys(params)
 .filter(key => key.startsWith("oauth"))
@@ -45,21 +51,25 @@ Object.keys(params)
 .map(key =>
 `${percentEncode(key)}="${percentEncode(params[key])}"`
 )
-.join(", ");
+.join(", ")
+);
 
-return authHeader;
 }
 
 export default async function handler(req, res){
 
 try{
 
-const { text } = req.body;
+const { text, image } = req.body;
 
-const url =
-"https://api.twitter.com/2/tweets";
+let mediaId = null;
 
-const oauthParams = {
+if(image){
+
+const mediaUrl =
+"https://upload.twitter.com/1.1/media/upload.json";
+
+const mediaParams = {
 
 oauth_consumer_key:
 process.env.TWITTER_API_KEY,
@@ -81,28 +91,95 @@ oauth_version:
 
 };
 
-const authHeader =
+const mediaAuth =
 generateOAuthHeader(
 "POST",
-url,
-oauthParams,
+mediaUrl,
+mediaParams,
 process.env.TWITTER_API_SECRET,
 process.env.TWITTER_ACCESS_SECRET
 );
 
-const response =
-await fetch(url, {
+const mediaResponse =
+await fetch(mediaUrl, {
 
 method:"POST",
 
 headers:{
-Authorization:authHeader,
+Authorization:mediaAuth,
+"Content-Type":
+"application/x-www-form-urlencoded"
+},
+
+body:
+`media_data=${encodeURIComponent(image)}`
+
+});
+
+const mediaData =
+await mediaResponse.json();
+
+mediaId = mediaData.media_id_string;
+
+}
+
+const tweetUrl =
+"https://api.twitter.com/2/tweets";
+
+const tweetParams = {
+
+oauth_consumer_key:
+process.env.TWITTER_API_KEY,
+
+oauth_nonce:
+Math.random().toString(36).substring(2),
+
+oauth_signature_method:
+"HMAC-SHA1",
+
+oauth_timestamp:
+Math.floor(Date.now()/1000).toString(),
+
+oauth_token:
+process.env.TWITTER_ACCESS_TOKEN,
+
+oauth_version:
+"1.0"
+
+};
+
+const tweetAuth =
+generateOAuthHeader(
+"POST",
+tweetUrl,
+tweetParams,
+process.env.TWITTER_API_SECRET,
+process.env.TWITTER_ACCESS_SECRET
+);
+
+const body = {
+text:text
+};
+
+if(mediaId){
+
+body.media = {
+media_ids:[mediaId]
+};
+
+}
+
+const response =
+await fetch(tweetUrl, {
+
+method:"POST",
+
+headers:{
+Authorization:tweetAuth,
 "Content-Type":"application/json"
 },
 
-body:JSON.stringify({
-text:text
-})
+body:JSON.stringify(body)
 
 });
 
